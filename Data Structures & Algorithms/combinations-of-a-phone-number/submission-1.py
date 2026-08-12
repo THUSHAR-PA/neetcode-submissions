50e6dfe01a8d50b6068dedb9a6cab336d771a1a9
Thushar P A <thusharpa@gmail.com>
2026-08-12 12:23:53 +0530

    Add: combinations-of-a-phone-number - submission-1

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res = []
+        hashmap ={"2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
+        def backtrack(i,substring):
+            if len(substring) == len(digits):
+                res.append(substring)
+                return
+            for c in hashmap[digits[i]]:
+                
+                backtrack(i+1,substring + c)
+        if digits:
+
+            backtrack(0,"")
+        return res
